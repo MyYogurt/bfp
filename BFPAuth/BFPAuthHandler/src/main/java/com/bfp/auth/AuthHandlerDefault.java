3499a9e06980c71c40ff48c662cca5b1b3341661
Panos Moisiadis <40012201+MyYogurt@users.noreply.github.com>
2024-07-22 01:11:00 -0700

    Pain

--- BFPAuth/BFPAuthHandler/src/main/java/com/bfp/auth/AuthHandlerDefault.java
+++ BFPAuth/BFPAuthHandler/src/main/java/com/bfp/auth/AuthHandlerDefault.java
@@ -2,11 +2,15 @@ package com.bfp.auth;
 
 import com.bfp.auth.model.InitiateAuthRequest;
 import com.bfp.auth.model.InitiateAuthResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Service;
 
 public class AuthHandlerDefault implements AuthHandler {
     @Override
-    public InitiateAuthResponse authenticate(InitiateAuthRequest initiateAuthRequest) {
-        InitiateAuthResponse response = new InitiateAuthResponse();
+    public InitiateAuthResponse authenticate(@NonNull final InitiateAuthRequest initiateAuthRequest) {
+        InitiateAuthResponse response = InitiateAuthResponse.builder()
+            .accessToken("dummy-access-token")
+            .build();
         return response;
     }
 }
--- BFPService/src/main/java/com/bfp/service/AuthService.java
+++ BFPService/src/main/java/com/bfp/service/AuthService.java
@@ -2,9 +2,11 @@ package com.bfp.service;
 
 import com.bfp.auth.AuthHandler;
 import com.bfp.auth.model.InitiateAuthRequest;
+import com.bfp.auth.model.InitiateAuthResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,7 +20,8 @@ public class AuthService {
     }
 
     @GetMapping("/authenticate")
-    public void authenticate(@NonNull InitiateAuthRequest initiateAuthRequest) {
-        authHandler.authenticate(initiateAuthRequest);
+    public InitiateAuthResponse authenticate(@NonNull @RequestBody final InitiateAuthRequest initiateAuthRequest) {
+        InitiateAuthResponse response = authHandler.authenticate(initiateAuthRequest);
+        return response;
     }
 }
