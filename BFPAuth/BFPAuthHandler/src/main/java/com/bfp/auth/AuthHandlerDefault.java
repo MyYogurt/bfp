69d6c79933e7557454957c2b5fa116f094df1143
Panos Moisiadis <40012201+MyYogurt@users.noreply.github.com>
2024-07-22 17:42:38 -0700

    Update model (#6)
    * Update model
    
    * Pain

--- BFPAuth/BFPAuthHandler/src/main/java/com/bfp/auth/AuthHandlerDefault.java
+++ BFPAuth/BFPAuthHandler/src/main/java/com/bfp/auth/AuthHandlerDefault.java
@@ -1,10 +1,16 @@
 package com.bfp.auth;
 
 import com.bfp.auth.model.InitiateAuthRequest;
+import com.bfp.auth.model.InitiateAuthResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Service;
 
 public class AuthHandlerDefault implements AuthHandler {
     @Override
-    public void authenticate(InitiateAuthRequest initiateAuthRequest) {
-
+    public InitiateAuthResponse authenticate(@NonNull final InitiateAuthRequest initiateAuthRequest) {
+        InitiateAuthResponse response = InitiateAuthResponse.builder()
+            .accessToken("dummy-access-token")
+            .build();
+        return response;
     }
 }
