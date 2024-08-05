5880318821409fccbcf43572f4492e910cc5a238
Panos Moisiadis <40012201+MyYogurt@users.noreply.github.com>
2024-08-04 17:24:35 -0700

    Fix CDK code

---
+++ BFPCDK/src/main/java/com/bfp/cdk/BFPServiceStatefulStack.java
@@ -0,0 +1,61 @@
+package com.bfp.cdk;
+
+import software.amazon.awscdk.CfnOutput;
+import software.amazon.awscdk.CfnOutputProps;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.ecr.IRepository;
+import software.amazon.awscdk.services.ecr.Repository;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
+import software.constructs.Construct;
+
+public class BFPServiceStatefulStack extends Stack {
+    public BFPServiceStatefulStack(final Construct parent, final String id) {
+        this(parent, id, null);
+    }
+
+    public BFPServiceStatefulStack(final Construct parent, final String id, final StackProps props) {
+        super(parent, id, props);
+
+        // Create VPC with a AZ limit of two.
+        Vpc vpc = Vpc.Builder.create(this, "BFPVPC")
+                .maxAzs(2)
+                .build();
+
+        // Create the ECS Service
+        Cluster cluster = Cluster.Builder.create(this, "BFPCluster")
+                .vpc(vpc)
+                .build();
+
+        IRepository ecrRepo = Repository.fromRepositoryName(this, "bfp", "bfpservicerepository");
+
+        // Use the ECS Network Load Balanced Fargate Service construct to create a ECS service
+        ApplicationLoadBalancedFargateService fargateService = ApplicationLoadBalancedFargateService.Builder.create(this, "MyFargateService")
+                .cluster(cluster)
+                .cpu(256)
+                .memoryLimitMiB(512)
+                .desiredCount(1)
+                .taskImageOptions(
+                        ApplicationLoadBalancedTaskImageOptions.builder()
+                                .image(ContainerImage.fromEcrRepository(ecrRepo, "latest"))
+                                .containerPort(8080)
+                                .build())
+                .publicLoadBalancer(true)
+                .build();
+
+        fargateService.getTargetGroup().configureHealthCheck(HealthCheck.builder()
+                .path("/actuator/health")
+                .healthyHttpCodes("200")
+                .build());
+
+        new CfnOutput(this, "LoadBalancerDNS", CfnOutputProps.builder()
+                .value(fargateService.getLoadBalancer().getLoadBalancerDnsName())
+                .description("Load balancer DNS")
+                .build());
+    }
+}
