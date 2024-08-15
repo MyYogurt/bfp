91f7ed9a5470fa18cbe1fad71be290045e12401a
Panos Moisiadis <40012201+MyYogurt@users.noreply.github.com>
2024-08-14 20:48:08 -0700

    Use app runner instead of ECS for simplicity

--- BFPCDK/src/main/java/com/bfp/cdk/BFPServiceStatefulStack.java
+++ BFPCDK/src/main/java/com/bfp/cdk/BFPServiceStatefulStack.java
@@ -3,19 +3,20 @@ package com.bfp.cdk;
 import software.amazon.awscdk.CfnOutput;
 import software.amazon.awscdk.CfnOutputProps;
 import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.services.apprunner.alpha.AutoScalingConfiguration;
+import software.amazon.awscdk.services.apprunner.alpha.Cpu;
+import software.amazon.awscdk.services.apprunner.alpha.EcrProps;
+import software.amazon.awscdk.services.apprunner.alpha.HealthCheck;
+import software.amazon.awscdk.services.apprunner.alpha.HttpHealthCheckOptions;
+import software.amazon.awscdk.services.apprunner.alpha.ImageConfiguration;
+import software.amazon.awscdk.services.apprunner.alpha.Memory;
+import software.amazon.awscdk.services.apprunner.alpha.Service;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.apprunner.alpha.Source;
 import software.amazon.awscdk.services.ecr.IRepository;
 import software.amazon.awscdk.services.ecr.Repository;
-import software.amazon.awscdk.services.ecs.Cluster;
-import software.amazon.awscdk.services.ecs.ContainerImage;
-import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
-import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
-import software.amazon.awscdk.services.elasticloadbalancingv2.Protocol;
 import software.constructs.Construct;
-
-import java.util.List;
 
 public class BFPServiceStatefulStack extends Stack {
     public BFPServiceStatefulStack(final Construct parent, final String id) {
@@ -25,46 +26,37 @@ public class BFPServiceStatefulStack extends Stack {
     public BFPServiceStatefulStack(final Construct parent, final String id, final StackProps props) {
         super(parent, id, props);
 
-        // Create VPC with a AZ limit of two.
-        Vpc vpc = Vpc.Builder.create(this, "BFPVPC")
-                .maxAzs(2)
-                .build();
+        IRepository repository = Repository.fromRepositoryName(this, "BFPRepository", "bfprepository");
 
-        // Create the ECS Service
-        Cluster cluster = Cluster.Builder.create(this, "BFPCluster")
-                .vpc(vpc)
-                .build();
-
-        IRepository ecrRepo = Repository.fromRepositoryName(this, "bfp", "bfpservicerepository");
-
-        // Use the ECS Network Load Balanced Fargate Service construct to create a ECS service
-        ApplicationLoadBalancedFargateService fargateService = ApplicationLoadBalancedFargateService.Builder.create(this, "MyFargateService")
-                .cluster(cluster)
-                .cpu(256)
-                .memoryLimitMiB(512)
-                .desiredCount(1)
-                .taskImageOptions(
-                        ApplicationLoadBalancedTaskImageOptions.builder()
-                                .image(ContainerImage.fromEcrRepository(ecrRepo, "latest"))
-                                .containerPort(8080)
-                                .build())
-                .healthCheck(software.amazon.awscdk.services.ecs.HealthCheck.builder()
-                        .command(List.of("CMD-SHELL", "curl -f http://localhost:8080/actuator/health || exit 1"))
+        Service bfpservice = Service.Builder.create(this, "BFPService")
+                .serviceName("BFPService")
+                .source(Source.fromEcr(EcrProps.builder()
+                    .repository(repository)
+                            .imageConfiguration(ImageConfiguration.builder()
+                                    .port(8080)
+                                    .build())
+                        .tagOrDigest("latest")
+                    .build()))
+                .autoDeploymentsEnabled(true)
+                .autoScalingConfiguration(AutoScalingConfiguration.Builder.create(this, "BFPAppRunnerAutoScalingConfiguration")
+                        .minSize(1)
+                        .maxSize(10)
+                        .maxConcurrency(100)
                         .build())
-                .publicLoadBalancer(true)
+                .cpu(Cpu.HALF_VCPU)
+                .memory(Memory.ONE_GB)
+                .healthCheck(HealthCheck.http(HttpHealthCheckOptions.builder()
+                                .path("/actuator/health")
+                                .timeout(Duration.seconds(5))
+                                .interval(Duration.seconds(10))
+                                .unhealthyThreshold(5)
+                                .healthyThreshold(1)
+                        .build()))
                 .build();
 
-        fargateService.getTargetGroup().configureHealthCheck(software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck.builder()
-                .path("/actuator/health")
-                .healthyHttpCodes("200")
-                .protocol(Protocol.HTTP)
-                .port("8080")
-                .interval(Duration.seconds(30))
-                .build());
-
-        new CfnOutput(this, "LoadBalancerDNS", CfnOutputProps.builder()
-                .value(fargateService.getLoadBalancer().getLoadBalancerDnsName())
-                .description("Load balancer DNS")
+        new CfnOutput(this, "BFPServiceURL", CfnOutputProps.builder()
+                .value(bfpservice.getServiceUrl())
+                .description("BFPService URL")
                 .build());
     }
 }
