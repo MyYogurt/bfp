2f37bad2e7395d131fb5cbed8b97ed392b80223b
Panos Moisiadis <40012201+MyYogurt@users.noreply.github.com>
2024-08-18 13:56:34 -0700

    Dev ops

--- BFPCDK/src/main/java/com/bfp/cdk/BFPApp.java
+++ BFPCDK/src/main/java/com/bfp/cdk/BFPApp.java
@@ -6,6 +6,7 @@ public class BFPApp {
     public static void main(String[] args) {
         App app = new App();
         new BFPServiceStatefulStack(app, "BFPService");
+        new DevOpsStack(app, "BFPDevOps");
         app.synth();
     }
 }
